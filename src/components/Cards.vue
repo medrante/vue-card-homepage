79af73b83761ec17e7bac58f0dacb82b8a356fa1
Trinia Medrano <tglmedrano@gmail.com>
2020-06-26 11:21:22 -0400

    Fix grid formatting for cards

--- src/components/Cards.vue
+++ src/components/Cards.vue
@@ -1,11 +1,17 @@
 <template>
   <v-main>
-    <v-row justify="space-around" :key="index" v-for="(tool, index) in tools">
-      <v-card :href="tool.link">
-        <v-card-title>{{ tool.name }}</v-card-title>
-        <v-card-text>{{ tool.description }}</v-card-text>
-      </v-card>
-    </v-row>
+    <v-item-group>
+      <v-container>
+        <v-layout row wrap>
+          <v-flex xs12 sm6 md4 lg3 v-for="(tool, index) in tools" :key="index">
+            <v-card :href="tool.link" class="text-xs-center ma-3">
+              <v-card-title>{{ tool.name }}</v-card-title>
+              <v-card-text>{{ tool.description }}</v-card-text>
+            </v-card>
+          </v-flex>
+        </v-layout>
+      </v-container>
+    </v-item-group>
   </v-main>
 </template>
 
